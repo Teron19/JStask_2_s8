55fde68c88f5ea136c6eed625adb94fd2d75bb38
Teron19 <vladmordasov0@gmail.com>
2018-12-18 22:41:41 +0200

    12

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-var uprostitel = document.getElementById("result");
-var total = 0, razniza = 0;
+"use sctrict" //чаще всего сейчас используют let 
+
+let total = 0,
+    difference = 0;
+let result = " ";
 
 for (var i = 1; i <=15; i++) {
-    var first = Math.floor((Math.random() * 6) + 1);
-    var second = Math.floor((Math.random() *6) +1);
+    var first = Math.floor((Math.random() * 6) + 1); // возвращяет найбольшее целое число Math.floor
+    var second = Math.floor((Math.random() *6) + 1); //
 
     if (i === 8 || i === 13) {
         continue;
     }
     console.log("good" + i);
 
-    uprostitel.innerHTML += "Первая кость: " + first  + " Вторая кость: " + second + "<br>" + " "
+    result += "Первая кость: " + first  + " Вторая кость: " + second + "<br>" + " ";
 
     if (first === second) {
-        uprostitel.innerHTML += "#Выпал дубль. Число: " + second + "<br>"
+        result += "#Выпал дубль. Число: " + second + "<br>";
     }
-    razniza = second - first;
+    difference = Math.abs(second - first);  //используем модуль Math.abs
     if (first < 3 && second > 4 || second < 3 && first > 4) {
-        
-        uprostitel.innerHTML += "Большой разброс между костями. Разница составляет: "+ razniza + "<br>" 
+        result += "Большой разброс между костями. Разница составляет: "+ razniza + "<br>";
     }
     total += first + second;
     console.log("Resultat: " + total);
 }
 
-uprostitel.innerHTML += (total > 100) 
-? ("Победа, вы набрали " + total + " очков") 
-: ("Вы проиграли, у вас " + total + " очков");
-  
+
+result += (total > 100) 
+                    ? ("Победа, вы набрали " + total + " очков") 
+                    : ("Вы проиграли, у вас " + total + " очков");
+ document.getElementById("result").innerHTML = difference;
